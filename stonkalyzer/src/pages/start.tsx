751baecd6827511ac8373c379f6984e296b23da3
Mihir Sahu <2002mihir@gmail.com>
2023-01-29 03:58:41 -0600

    Modify start page structure

--- stonkalyzer/src/pages/start.tsx
+++ stonkalyzer/src/pages/start.tsx
@@ -4,11 +4,6 @@ import MiniStock from "@/components/MiniStock";
 export const start = () => {
   return (
     <div style={{ flexDirection: "row", display: "flex" }}>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <MiniStock label="NASDAQ" symbol="NDAQ" />
-        <MiniStock label="S&P 500" symbol="SPY" />
-        <MiniStock label="Dow Jones" symbol="DJIA" />
-      </div>
       <Form />
       <div style={{ display: "flex", flexDirection: "column" }}>
         <MiniStock label="NASDAQ" symbol="NDAQ" />
